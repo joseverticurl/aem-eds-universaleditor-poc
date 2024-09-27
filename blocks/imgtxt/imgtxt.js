61b07c5c3db8445b203eeb88ac64692d8f7123b3
joseverticurl <joseph.ambrose@verticurl.com>
2024-09-27 21:42:20 +0530

    rty

--- blocks/imgtxt/imgtxt.js
+++ blocks/imgtxt/imgtxt.js
@@ -7,7 +7,7 @@ function applySmartCrop(image) {
 
 export default async function decorate(block) {
   console.log('Block dataset image :', block.dataset.image);
-  console.log('image attribute :: ',block.getAttribute('data-image'));
+  console.log('image attribute :: ', block.getAttribute('data-image'));
   console.log('Block dataset smartCrop :', block.dataset.smartCrop);
   console.log('Block dataset text :', block.dataset.text);
   console.log('Block dataset alignment :', block.dataset.alignment);
