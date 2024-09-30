63c0242eeea827a3ace9403891eaee154ca09806
joseverticurl <joseph.ambrose@verticurl.com>
2024-09-30 19:17:21 +0530

    trtt

--- blocks/imgtxt/imgtxt.js
+++ blocks/imgtxt/imgtxt.js
@@ -5,24 +5,23 @@ function applySmartCrop(image) {
   // Desktop smart crop name
   const desktop = 'fb600x400';
   // Mobile smart crop name
-  const mobile="Test250X250";
+  const mobile = 'Test250X250';
   const url = image.src;
   const authoredBaseUrl = 'https://author-p35060-e135954.adobeaemcloud.com/content/dam/aem-eds-universaleditor-training';
   // Remove the base URL by slicing the string
   let result = url.substring(authoredBaseUrl.length);
   result = result.substring(0, result.length - 4);
- 
   let finalUrl = `${dmBaseUrl}/${result}:${desktop}`;
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    finalUrl=`${finalUrl}:${mobile}`; // Mobile devices
-  } else if (window.matchMedia("(min-width: 768px) and (max-width: 1024px)").matches) {
-    finalUrl=`${finalUrl}:${mobile}`; // Tablet devices
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    finalUrl = `${finalUrl}:${mobile}`; // Mobile devices
+  } else if (window.matchMedia('(min-width: 768px) and (max-width: 1024px)').matches) {
+    finalUrl = `${finalUrl}:${mobile}`; // Tablet devices
   } else {
-    finalUrl=`${finalUrl}:${desktop}`; // Tablet devices
+    finalUrl = `${finalUrl}:${desktop}`; // Tablet devices
   }
 
   // Desktop scene7 url  : https://s7ap1.scene7.com/is/image/wpppartnersandbox/peak-performance:fb600x400?fmt=png-alpha
- // const finalUrl = `${dmBaseUrl}/${result}:${desktop}`;
+  // const finalUrl = `${dmBaseUrl}/${result}:${desktop}`;
   console.log(finalUrl);
 }
 
