b63e21998b97e47d9797209ad1ef4e5dede66813
joseverticurl <joseph.ambrose@verticurl.com>
2024-09-30 19:24:38 +0530

    popo

--- blocks/imgtxt/imgtxt.js
+++ blocks/imgtxt/imgtxt.js
@@ -15,21 +15,18 @@ function applySmartCrop(image) {
   if (window.matchMedia('(max-width: 767px)').matches) {
     finalUrl = `${finalUrl}:${mobile}`; // Mobile devices
   } else if (window.matchMedia('(min-width: 768px) and (max-width: 1024px)').matches) {
-    finalUrl = `${finalUrl}:${mobile}`; // Tablet devices
+    finalUrl = `${finalUrl}:${mobile}`; // mobile devices
   } else {
     finalUrl = `${finalUrl}:${desktop}`; // Tablet devices
   }
   image.src = finalUrl;
-  // Desktop scene7 url  : https://s7ap1.scene7.com/is/image/wpppartnersandbox/peak-performance:fb600x400?fmt=png-alpha
-  // const finalUrl = `${dmBaseUrl}/${result}:${desktop}`;
-  console.log(image.src);
 }
 
 export default async function decorate(block) {
-  console.log('Block dataset image :', block.querySelector('[data-aue-prop="image"]'));
-  console.log('Block dataset smartCrop :', block.querySelector('[data-aue-prop="smartCrop"]').textContent);
-  console.log('Block dataset text :', block.querySelector('[data-aue-prop="text"]'));
-  console.log('Block dataset alignment :', block.querySelector('[data-aue-prop="alignment"]'));
+  // console.log('Block dataset image :', block.querySelector('[data-aue-prop="image"]'));
+  // console.log('Block dataset smartCrop :', block.querySelector('[data-aue-prop="smartCrop"]').textContent);
+  // console.log('Block dataset text :', block.querySelector('[data-aue-prop="text"]'));
+  // console.log('Block dataset alignment :', block.querySelector('[data-aue-prop="alignment"]'));
   // Get the image, text, and alignment properties from the block
   const image = block.querySelector('[data-aue-prop="image"]');
   // Apply smart crop if the image element exists
