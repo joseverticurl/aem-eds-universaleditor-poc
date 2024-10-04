64db948d3ac6fb657c04684369642c8bde7c3baa
joseverticurl <joseph.ambrose@verticurl.com>
2024-10-04 10:34:01 +0530

    ffee

--- scripts/aascript.js
+++
@@ -1,9 +0,0 @@
-window.adobeDataLayer = window.adobeDataLayer || [];
-window.adobeDataLayer.push({
-    'event': 'page-view',
-	'eventInfo': {
-		'pageContext': {
-			'pageName': 'home-page'
-		}
-	}
-});
