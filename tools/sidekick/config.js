8f4fd18283b52d23afd27888096a08263b915461
joseverticurl <joseph.ambrose@verticurl.com>
2024-10-13 23:18:34 +0530

    3656

---
+++ tools/sidekick/config.js
@@ -0,0 +1,24 @@
+window.hlx.initSidekick({
+  project: 'aem-eds-universaleditor-poc',
+  host: 'localhost',
+  plugins: [
+    {
+      "id": "aemedit",
+      "title": "Edit-",
+      "environments": [ "dev", "preview", "live" ],
+      "event": "aemedit"
+    },
+    {
+        id: 'disable-publish',
+        title: 'Disable Publish',
+        environments: ['dev', 'preview', 'live'],
+        condition: () => true, // Always true to disable publish button
+        button: async () => {
+          const publishButton = document.querySelector('[aria-label="Publish"]');
+          if (publishButton) {
+            publishButton.style.display = 'none';
+          }
+        }
+    }  
+  ],
+});
